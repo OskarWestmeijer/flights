ff83ee72f3ba2c1e4d3f949ce1e46c447ee9d4cc
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-11-26 19:41:29 +0100

    fix playwright test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page loads globe', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.locator('#helloWorld')).toBeVisible();
 });
