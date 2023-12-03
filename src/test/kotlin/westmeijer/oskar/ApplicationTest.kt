c66a615691f2e9946215843bd643f0a795cfdf4f
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-12-03 21:27:40 +0100

    feat: add workflow

--- src/test/kotlin/westmeijer/oskar/ApplicationTest.kt
+++ src/test/kotlin/westmeijer/oskar/ApplicationTest.kt
@@ -1,12 +1,14 @@
 package westmeijer.oskar
 
 import io.ktor.server.testing.*
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     @Test
+    @Ignore
     fun testRoot() = testApplication {
         assertEquals("a", "a")
     }
