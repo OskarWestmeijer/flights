58a43442a6529314f62378f6d6ba252edc47cedf
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-12-04 19:20:07 +0100

    feat: add frontend deployment

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('http://localhost:8080/flight-routes');
+	const res = await fetch('https://api.maps.oskar-westmeijer.com/flight-routes');
 	const data = await res.json();
 
 	return data;
