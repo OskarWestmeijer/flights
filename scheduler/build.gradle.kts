52c88ada634e387a34251233546bd6e89511e19e
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-03-01 14:19:02 +0100

    fix: building jar

--- scheduler/build.gradle.kts
+++ scheduler/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    application
 }
 
 group = "westmeijer.oskar"
@@ -11,6 +12,8 @@ repositories {
 dependencies {
     implementation("io.lettuce:lettuce-core:6.3.1.RELEASE")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
+    implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("ch.qos.logback:logback-classic:1.2.6")
     implementation("com.typesafe:config:1.4.3")
 
@@ -26,10 +29,13 @@ kotlin {
 }
 
 tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
     archiveBaseName.set("maps-scheduler")
     manifest {
         attributes(
-            "Main-Class" to "Main.kt"
+            "Main-Class" to "westmeijer.oskar.MainKt"
         )
     }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
