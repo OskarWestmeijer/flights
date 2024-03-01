62dc1036f0961a32ceac4148dc9fcd3831038980
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-03-01 14:01:29 +0100

    include scheduler in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ compose_down="docker compose -f cprod.yml down"
 list_images="docker images"
 rm_latest_api="docker image rm oskarwestmeijer/maps-api:latest"
 rm_latest_ui="docker image rm oskarwestmeijer/maps-ui:latest"
+rm_latest_scheduler="docker image rm oskarwestmeijer/maps-scheduler:latest"
 compose_up="docker compose -f cprod.yml up -d"
 proxy_restart="docker restart reverse-proxy"
 
@@ -13,6 +14,7 @@ $compose_down
 $list_images
 $rm_latest_api
 $rm_latest_ui
+$rm_latest_scheduler
 $compose_up
 $proxy_restart
 
