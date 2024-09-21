1b49e57a7b9925793c02dc35cd152f4b06d4e6cd
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-09-21 19:45:36 +0200

    feat: add tests

--- src/test/kotlin/westmeijer/oskar/models/client/DepartingFlightTest.kt
+++ src/test/kotlin/westmeijer/oskar/models/client/DepartingFlightTest.kt
@@ -1,9 +1,11 @@
 package westmeijer.oskar.models.client
 
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class DepartingFlightTest {
 
@@ -33,4 +35,40 @@ class DepartingFlightTest {
         assertEquals(expectedDepartingFlight, departingFlight)
     }
 
+    @Test
+    fun `test deserialization of empty destinationAirport3LCode`() {
+        // Arrange
+        val json = """{"destinationAirport3LCode":""}"""
+        val expectedDepartingFlight = DepartingFlight("")
+
+        // Act
+        val departingFlight = Json.decodeFromString<DepartingFlight>(json)
+
+        // Assert
+        assertEquals(expectedDepartingFlight, departingFlight)
+    }
+
+    @Test
+    fun `test serialization of empty destinationAirport3LCode`() {
+        // Arrange
+        val departingFlight = DepartingFlight("")
+        val expectedJson = """{"destinationAirport3LCode":""}"""
+
+        // Act
+        val jsonResult = Json.encodeToString(departingFlight)
+
+        // Assert
+        assertEquals(expectedJson, jsonResult)
+    }
+
+    @Test
+    fun `test deserialization with missing destinationAirport3LCode`() {
+        // Arrange
+        val json = """{}""" // missing the "destinationAirport3LCode" field
+
+        // Act & Assert
+        assertFailsWith<SerializationException> {
+            Json.decodeFromString<DepartingFlight>(json)
+        }
+    }
 }
