14ac124ebe3d85953536bae3e15a24541de69f55
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-09-22 12:32:59 +0200

    feat: rename to flights and connections

--- src/main/kotlin/westmeijer/oskar/redis/ConnectionsCacheCodec.kt
+++ src/main/kotlin/westmeijer/oskar/redis/ConnectionsCacheCodec.kt
@@ -3,20 +3,20 @@ package westmeijer.oskar.redis
 import io.lettuce.core.codec.RedisCodec
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
-import westmeijer.oskar.models.server.FlightRoute
+import westmeijer.oskar.models.server.Connection
 import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
 
 
-internal class FlightRoutesCacheCodec : RedisCodec<String, List<FlightRoute>> {
+internal class ConnectionsCacheCodec : RedisCodec<String, List<Connection>> {
     private val charset: Charset = Charset.forName("UTF-8")
 
     override fun decodeKey(bytes: ByteBuffer): String {
         return charset.decode(bytes).toString()
     }
 
-    override fun decodeValue(bytes: ByteBuffer): List<FlightRoute>? {
+    override fun decodeValue(bytes: ByteBuffer): List<Connection>? {
         try {
             val jsonString = charset.decode(bytes).toString()
             return Json.decodeFromString(jsonString)
@@ -29,10 +29,10 @@ internal class FlightRoutesCacheCodec : RedisCodec<String, List<FlightRoute>> {
         return charset.encode(key)
     }
 
-    override fun encodeValue(value: List<FlightRoute>): ByteBuffer? {
+    override fun encodeValue(value: List<Connection>): ByteBuffer? {
         try {
 
-            val jsonString = Json.encodeToString(ListSerializer(FlightRoute.serializer()), value)
+            val jsonString = Json.encodeToString(ListSerializer(Connection.serializer()), value)
             return ByteBuffer.wrap(jsonString.toByteArray(charset))
         } catch (e: IOException) {
             e.printStackTrace()
