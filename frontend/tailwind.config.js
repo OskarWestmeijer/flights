06b220f5a2b237c200ec7b24a034b9019a28da51
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2025-09-20 14:07:05 +0200

    feat: remove old daisyui reference

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,24 +3,5 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
-	},
-	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					// Navy #0a192f
-					// light navy #112240
-					// lightest navy #233554
-					// Green #64ffda
-					primary: '#112240',
-					secondary: '#233554',
-					accent: '#64ffda',
-					accentlight: '#64ffda',
-					neutral: '#c5fff0',
-					'base-100': '#ffffff'
-				}
-			}
-		]
-	},
-	plugins: [require('daisyui')]
+	}
 };
